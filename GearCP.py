2b9d7475a6e7726f01a6a156e3696612a5b1c461
Carlos Fernandes <cfernandes@fe.up.pt>
2021-01-04 13:18:29 +0000

    Update GearCP.py

--- GearCP.py
+++ GearCP.py
@@ -1,7 +1,7 @@
 import numpy as np
 from GearC import gears, MAAG, contact, LoadStage, oils, material, bearings, plot
 ## GEAR SELECTION ##################################################################
-gear = 'RFT'                    # 'C40',  '501',  '701',  '951',  'TPA'
+gear = '501'                    # 'C40',  '501',  '701',  '951',  'TPA'
 mat = ['STEEL', 'STEEL']        # 'PEEK',  'PA66',  'STEEL' (20MnCr5),  'ADI'
 ## TYPE OF GEAR ####################################################################
 alpha, beta, m, z, x, b, dsh, Ra, Rq = gears.gtype(gear)
